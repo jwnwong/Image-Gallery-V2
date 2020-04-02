a6c1112ca7d46c17621ff9012abf56303ac12a8f
Jason Wong <jwnwong@hotmail.com>
2020-04-02 14:07:19 +0800

    Initial Commit

---
+++ Image Gallery V2/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  Image Gallery V2
+//
+//  Created by Jason Wong on 2/4/2020.
+//  Copyright © 2020 Jason Wong. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
